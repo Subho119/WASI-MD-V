db4ae041a6d60f939aa39283616e6e17e75452b5
Subho119 <159559837+Subho119@users.noreply.github.com>
2024-11-17 08:55:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.THUMB_IMAGE =
   "https://telegra.ph/file/1f4e6e1cc2ed1662adb36.jpg";
 global.devs = "923135673658";
 global.sudo = process.env.SUDO || "923192173398";
-global.owner = process.env.OWNER_NUMBER || "923192173398";
+global.owner = process.env.OWNER_NUMBER || "918945999655";
 global.style = process.env.STYLE || "3";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 (global.disablegroup = process.env.DISABLE_GROUPS || "false"),
@@ -61,7 +61,7 @@ global.scan = "https://pair-qr-wasi-md.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "";
+  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYUdPQjdhVUVHbS9VeFpWdm1MUkxMclVXNVh4bWQvTGh6MjZubHRuMXVIcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic3A4aWJDb2lkVi9KdXZHL00zdEljRVlkSzIrRzhVdDc2VlVYZnFQMDhRWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJHSXBoYzJhWmRiamt3cUhtOU95cHBCNGJTQVEwQzFmR21ZMy9abVlCY2tnPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKK05XSWR6WEdSSVM5Qy9rYk1oS3A1dk9mNWc2bTZ2V1FWTHlJcHJjcUZjPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjhNMlBBdnZWcUZJMCs1bnJxMHZweUFwNmo3RUJhT3JzVVBWdTNJR1pEWEU9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImZCeE5xTkhqRjZ6a1BzRXJOSFFyTHpQQ1V6Rkt0TFJQS2JlWWMvaGVsekk9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0c1cXBPSlI5UnhLVzdKVDlnbWpMQzdqeHZOcWlsbUlicjJlZXBLYkhFdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRG9rMkFMN0ZOTWhYcDUycVZzRHMzV2VFZ1pUQ1RFUmh6TnI3d2cvRTBXVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjcvQUlXQlFoMTlHcE1pcHUwRVQ4Ny9jWmZXZjdWdVV0RTZvalE4ZFcrMDdtMDZIOU5iRElMd1dpTjJZODBZT25qYmM1SmVGTGFvU1A1aUhSZDUwZERRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjksImFkdlNlY3JldEtleSI6InVNSjJjSFdBb0JsZVdIZi9lUzFZNXgzeHNEWDhTcWMvRkNXelhuRTlEWEk9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6ImFZWTRmS1g4UnJTRm1pR2hka3FkdFEiLCJwaG9uZUlkIjoiNGRiN2I1ZWItNjJiYy00MmM4LTkyMTUtODBjNmM0OTIzNjA1IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImVKK0MzL202c3AvWGxybXBpbFFjM2pKYURCOD0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJOeVpSNGhvQmNtRFR4YnJOL2V4eFNLdXgyWU09In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiQkJQOEZWVzIiLCJtZSI6eyJpZCI6IjkxODk0NTk5OTY1NTo1MUBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiJTdWJobyJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDS3V0cXBZSEVOWEI1YmtHR0FjZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoielZmZ01NN25UMGxBYTNrRHp4Q3N2azFrU082alk5WlE1czBvY3BnVFoyaz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiWUprT0Mzc1oxVXBwS3R5QU9kaEVmcUorR0s4MlZpaVYrMHp4bUlGemdEemd6MHkyaGNDTWNMZVo1eVd5RjNTYUNvR0Nmb3RkbkNpaGRNOG1BZzhqQ1E9PSIsImRldmljZVNpZ25hdHVyZSI6ImtOV3lUTDRoSHA5c2loOUJ4c21NSVAyWTBDOWNJc0ZXTU1yT0oxZXRrSVhlNlBGRmNTLzNmbmZ5ckd0TE15SE9iZTdoN0RIMWQ5WVFTYW8yRXBQdkJ3PT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiOTE4OTQ1OTk5NjU1OjUxQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmMxWDRERE81MDlKUUd0NUE4OFFyTDVOWkVqdW8yUFdVT2JOS0hLWUUyZHAifX1dLCJwbGF0Zm9ybSI6InNtYmEiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MzE4MTM2MDV9";
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ".",
@@ -82,7 +82,7 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
   AUTOREACTION: process.env.AUTOREACTION || process.env.AUTOREACTION || "true",
   LANG: (process.env.THEME || "WASI").toUpperCase(),
 };
